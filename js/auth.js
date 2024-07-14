870c82bb0aebe296fbbd2d62b2ab167f19e32e60
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-07-14 22:28:00 +0600

    user registration done

--- js/auth.js
+++ js/auth.js
@@ -1,37 +1,36 @@
 const handleRegister = (event) => {
-    event.preventDefault()
-    const form = document.getElementById("register-form")
-    const formData  = new FormData(form)
-    
+    event.preventDefault();
+    const form = document.getElementById("register-form");
+    const formData  = new FormData(form);
+    console.log(formData.get("profile_image")?.file)
     const registerData = {
-        username : formData.get("username"),
-        email : formData.get("email"),
-        first_name : formData.get("first_name"),
-        last_name : formData.get("last_name"),
-        
-        password : formData.get("password"),
-        confirm_password : formData.get("password2"),
+        username: formData.get("username"),
+        email: formData.get("email"),
+        first_name: formData.get("first_name"),
+        last_name: formData.get("last_name"),
+        password: formData.get("password"),
+        confirm_password: formData.get("password2"),
+        user_type: formData.get("user_type"),
+        phone: formData.get("phone"),
+        image: formData.get("profile_image") 
     };
-    console.log(registerData);
-
-    fetch("https://amar-kotha.onrender.com/users/register/", {
-        method : "POST",
-        headers : {
+    console.log(registerData)
+    fetch("http://127.0.0.1:8000/users/register/", {
+        method: "POST",
+        headers: {
             "Content-Type": "application/json",
         },
-        body : JSON.stringify(registerData),
+        body: JSON.stringify(registerData),
     })
     .then((res) => res.json())
-    .then((data)=> {
-        console.log(data);
-        const accountData = {
-            user : data.user,
-            user_type : formData.get("user_type"),
-            profile_image : formData.get("profile_image"),
-            phone : formData.get("phone"),
-        }
-        console.log(accountData);
+    .then((data) => {
 
+        console.log(data);
+        if (data.error) {
+            // error show korben
+        } else {
+            // sucess msg dekhaben
+        }
     })
-    .then((err) => console.log(err))
-}
+    .catch((err) => console.log("error::",err))
+};
